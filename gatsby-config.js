513fecf35644ae5dd412187eb491194324da3d3c
shotaro chiba <s@pws.jp>
2020-10-25 11:26:33 +0900

    add: docs framework

---
+++ gatsby-config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  siteMetadata: {
+    title: `UNiD Docs`,
+    description: ``,
+    keywords: [`gatsby`, `theme`, `react`],
+    author: `CollaboGate`,
+    siteUrl: `https://gatsby-starter-catalyst.netlify.app`, //Change to you site address, required for sitemap.xml and robots.txt file among other things
+    menuLinks: [
+      {
+        name: `Left Link`,
+        link: `/page-1`,
+        type: `internal`, //internal or anchor
+        location: `left`,
+      },
+    ],
+    socialLinks: [
+      {
+        name: `Github`,
+        link: `https://www.github.com/getunid`,
+        location: `all`, //Options are "all", "header", "footer"
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-catalyst-core`,
+      options: {
+        //Default options are:
+        // contentPath: `content/pages`,
+        // assetPath: `content/assets`,
+        // displaySiteLogo: true,
+        // displaySiteTitle: true,
+        // displaySiteLogoMobile: true,
+        // displaySiteTitleMobile: true,
+        // invertLogo: false,
+        // useStickyHeader: false,
+        // useSocialLinks: true,
+        // useColorMode: true,
+        // useKatex: false,
+        // footerContentLocation: "left", // "left", "right", "center"
+        // remarkImagesWidth: 1440,
+      },
+    },
+    `gatsby-theme-catalyst-header-side`, // Try `gatsby-theme-catalyst-header-side`
+    `gatsby-theme-catalyst-footer`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-catalyst`,
+        short_name: `catalyst`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#cccccc`,
+        display: `minimal-ui`,
+        icon: `content/assets/catalyst-site-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
